a786e9b25ff1dccef302d4d93a93b9b0b436ce6e
maximog <maximo.gijon@gmail.com>
2020-01-17 13:41:52 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,105 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import moment from 'moment';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const timeFormat = "YYYY-MM-DDTHH:mm";
+
+const initialState = {
+    timeTillDate: undefined,
+    months: undefined,
+    days: undefined,
+    hours: undefined,
+    minutes: undefined,
+    seconds: undefined
 }
 
-export default App;
+class Countdown extends Component {
+  constructor() {
+    super();
+    this.state = initialState;
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      // const { timeTillDate, timeFormat } = this.props;
+      const then = moment(this.state.timeTillDate, timeFormat);
+      const now = moment();
+      const countdown = moment(then - now);
+      const months = countdown.subtract(1, 'months').format('MM');
+      const days = countdown.format('D');
+      const hours = countdown.format('HH');
+      const minutes = countdown.format('mm');
+      const seconds = countdown.format('ss');
+
+      this.setState({ months, days, hours, minutes, seconds });
+
+
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    if(this.interval) {
+      clearInterval(this.interval);
+    }
+  }
+
+  onDateChange = (event) => {
+    this.setState({ timeTillDate: event.target.value })
+    // console.log(event.target.value)
+
+} 
+
+
+  render() {
+    const { timeTillDate, months, days, hours, minutes, seconds } = this.state;
+
+    return (
+      <div> 
+        <input
+          id="date"
+          type='datetime-local'
+          onChange={this.onDateChange}
+          // defaultValue= {moment().format('YYYY-MM-DDTHH:mm')}
+        />
+        <div>Insert Date for Countdown</div>
+      
+      {timeTillDate ? 
+       <div>
+          <h1>Countdown</h1>
+          <div className='countdown-wrapper'>
+            <div className='countdown-item'>
+              <span>Selected End Date: </span>
+              {moment(timeTillDate).format("dddd, MMMM Do YYYY, h:mm:ss a")}
+            </div> 
+            <div className='countdown-item'>
+              {months}
+              <span> Months</span>
+            </div>
+            <div className='countdown-item'>
+              {days}
+              <span> Days</span>
+            </div>
+            <div className='countdown-item'>
+              {hours}
+              <span> Hours</span>
+            </div>
+            <div className='countdown-item'>
+              {minutes}
+              <span> Minutes</span>
+            </div>
+            <div className='countdown-item'>
+              {seconds}
+              <span> Seconds</span>
+            </div>
+          </div>
+        </div>
+      : <div>(No Date selected)</div>}
+      </div>
+    );
+  }
+
+}
+
+
+
+export default Countdown;
